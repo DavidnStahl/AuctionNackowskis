f5ba409cdf118603ba0b5cc1eb1960f170789ac3
47720480 <david.n.stahl@gmail.com>
2020-03-27 12:47:50 +0100

    Login fungerar

--- src/Components/ShowAuctionsList.js
+++ src/Components/ShowAuctionsList.js
@@ -16,44 +16,6 @@ const ShowAuctionsList = () => {
     useEffect(() => {
     },[AuctionsToShow,setAuctionsToShow])
 
-   /* return (
-        <div className="container text-center">
-        {Här har du ett exemple marwan hur du får ut titlarna}
-           {AuctionsToShow.map((auction) =>{
-               return <p>{auction.Titel}</p>
-           })}
-        </div>
-        <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
-    )*/
     return(
         <div className="container text-center">
 
