9129863b0e78d6fdec5528cd4031a27c33f2f2c2
salleee salman <thufek@hotmail.com>
2020-03-30 12:32:09 +0200

    DetailView hämta data test

--- src/Components/DetailViewAuction.js
+++ src/Components/DetailViewAuction.js
@@ -2,17 +2,29 @@ import React, { useContext, useEffect, useState } from "react";
 import { DetailViewAuctionContext } from "../Contexts/DetailViewAuctionContext";
 
 const DetailViewAuction = props => {
+  const [isShow, setIsShow] = [props.isShowing];
+  const [auctionDetails, setAuctionDetails] = useState([]);
+
   const [
     DetailDataForAuction,
     setDetailDataForAuction,
     BiddingDataForAuction,
     setBiddingDataForAuction,
-    getSelectedAuctionData
+    getSelectedAuctionData,
+    getDataToAuctionDetailList
   ] = useContext(DetailViewAuctionContext);
 
-  useEffect(() => {
-    getSelectedAuctionData(props.id);
-  }, []);
+  /*   useEffect(() => {
+    setAuctionDetails(GetDetails(props.isShowing));
+    console.log(auctionDetails);
+  }, []); */
+
+  const GetDetails = async bolean => {
+    if (bolean === false) {
+      return await getDataToAuctionDetailList;
+    }
+    return null;
+  };
 
   //Kasta in en useEffect? som hämtar datat, inte när komponenten renderas första gången, men när motsvarande view details knapp i parent (ShowAucttionList) trycks
 
@@ -20,7 +32,9 @@ const DetailViewAuction = props => {
   return (
     <tr hidden={!props.isShowing}>
       <td colSpan="6">
-        <div>ID: {props.id}</div>
+        <div>
+          ID: {props.id} , detalj1 {auctionDetails}
+        </div>
       </td>
     </tr>
   );
