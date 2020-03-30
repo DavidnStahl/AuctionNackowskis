d7cdf909e847df96b02fb9c53c1578f9267a1dfb
47720480 <david.n.stahl@gmail.com>
2020-03-30 16:12:23 +0200

    dr

--- src/Components/DetailViewAuction.js
+++ src/Components/DetailViewAuction.js
@@ -1,5 +1,38 @@
 import React, { useContext, useEffect, useState } from "react";
 import { DetailViewAuctionContext } from "../Contexts/DetailViewAuctionContext";
+
+const DetailViewAuction = props => {
+  const [auctionListItemData, setauctionListItemData] = useState([]);
+  const [ListItemVersion, setListItemVersion] = useState([]);
+
+  const [
+    DetailDataForAuction,
+    setDetailDataForAuction,
+    BiddingDataForAuction,
+    setBiddingDataForAuction,
+    getSelectedAuctionData,
+    getDataToAuctionDetailList
+  ] = useContext(DetailViewAuctionContext);
+
+  useEffect(() => {
+    GetDetails(props.isShowing);
+  }, [props.isShowing]);
+
+  useEffect(() => {
+
+    console.log(auctionListItemData[0])
+    if(auctionListItemData[0] !== undefined){
+      setListItemVersion(() => {
+        console.log(auctionListItemData[0].SkapadAv)
+        console.log(sessionStorage.getItem("user"))
+        if(auctionListItemData[0].SkapadAv === sessionStorage.getItem("user") && auctionListItemData[1].length === 0){
+          return ("ListState: egen auction som är tom och går att ta bort och uppdatera")
+        }else if(auctionListItemData[0].SkapadAv === sessionStorage.getItem("user") && auctionListItemData[1].length !== 0){
+          return ("ListState: egen auction som har bud och går inte att ta bort eller uppdatera")
+        }else if(auctionListItemData[0].SkapadAv !== sessionStorage.getItem("user")){
+          return ("ListState: annans auction som går att buda på")
+import React, { useContext, useEffect, useState } from "react";
+import { DetailViewAuctionContext } from "../Contexts/DetailViewAuctionContext";
 import DetailItem from "./DetailItem";
 
 const DetailViewAuction = props => {
@@ -49,7 +82,11 @@ const DetailViewAuction = props => {
     if (bolean === true) {
       let data = await getDataToAuctionDetailList(props.id);
       setauctionListItemData(data);
+
+      console.log("lal", auctionListItemData)
+
       console.log("lal", auctionListItemData);
+
     }
   };
 
